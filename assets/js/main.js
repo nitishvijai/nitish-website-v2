320df64bcee89d0cc348b16d729fd58e33a53287
nitishv <nitishv@umich.edu>
2020-05-19 23:11:32 -0400

    End of day commit - fixed project page

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-(function($){
-    $(document).mousemove(
-      function(e){
-        var X = e.pageX;
-        var Y = e.pageY;
-        $('body').css('background','radial-gradient(circle closest-side at '+X+'px '+Y+'px, #fff -10%, #121, #000 50%)');
-      }
-    );
-  })(jQuery);
